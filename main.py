3701e3f46b80b7a61b55801f0b85be009e361f59
MrBasuraHead <papastavropoulos.k@gmail.com>
2025-06-11 20:59:43 -0400

    D: Count letters

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import get_book_text
 
 from stats import count_words
+
+from stats import count_letters
      
 def main():
-    count_words("/home/kp/workspace/github.com/ppstvrplsk/bookbot/books/frankenstein.txt")
+    count_letters("/home/kp/workspace/github.com/ppstvrplsk/bookbot/books/frankenstein.txt")
 
 main()
+
+
+
+
 
 
 
--- stats.py
+++ stats.py
@@ -7,3 +7,17 @@ def count_words(filepath):
      with open(filepath) as f:
     # do something with f (the file) here
         print (f"{(len(f.read().split()))} words found in the document")
+
+def count_letters(filepath):
+    with open(filepath) as f:
+        file_content = f.read()
+        counter = {}
+        for i in file_content:
+            x = i.lower()
+            if x in counter:
+                counter[x] += 1
+            else:
+                counter[x] = 1
+        print (counter)
+                
+
