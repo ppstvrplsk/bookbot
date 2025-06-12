9f5a403a6e8688df65e5983b5761ef0b1e781e1f
MrBasuraHead <papastavropoulos.k@gmail.com>
2025-06-11 20:34:46 -0400

    C: stats.py file for functions

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-    # do something with f (the file) here
-        file_content = f.read()
+from stats import get_book_text
 
-def count_words(filepath):
-     with open(filepath) as f:
-    # do something with f (the file) here
-        print (f"{(len(f.read().split()))} words found in the document")
+from stats import count_words
      
 def main():
     count_words("/home/kp/workspace/github.com/ppstvrplsk/bookbot/books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,9 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+    # do something with f (the file) here
+        file_content = f.read()
+
+def count_words(filepath):
+     with open(filepath) as f:
+    # do something with f (the file) here
+        print (f"{(len(f.read().split()))} words found in the document")
